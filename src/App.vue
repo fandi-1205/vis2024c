9ffa76358e93210198a9053838c2427c4bf70a44
gaoyu030 <15165027938@163.com>
2024-06-22 15:13:19 +0800

    pull new branch

--- src/App.vue
+++ src/App.vue
@@ -57,23 +57,168 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMap from './components/mainMap';
+import LeftNav from './components/leftNav/';
+import KnowledgeMasteryVue from './components/knowledageMastery/';
+import HourHeat from './components/hourHeat/';
+import StudyOrder from './components/studyOrder/';
+import ProgressChart from './components/progressChart/';
+import StudentScores from './components/studentScores/';
+import TopNav from './components/topNav/';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TopNav,
+    LeftNav,
+    MainMap,
+    KnowledgeMasteryVue,
+    HourHeat,
+    StudyOrder,
+    ProgressChart,
+    StudentScores,
+  },
+  data() {
+    return {
+      axesData: [],
+    };
+  },
+  mounted() {
+    this.sendButtonInfoToBackend('');
+  },
+  methods: {
+    sendButtonInfoToBackend(buttonName) {
+      if (buttonName) {
+        axios
+          .get('http://127.0.0.1:3001/student2/sort', {
+            params: {
+              sortBy: buttonName,
+            },
+          })
+          .then((response) => {
+            console.log('请求成功，后端响应：', response.data);
+            // 在这里处理成功的逻辑，比如更新 UI
+            this.axesData = response.data;
+            this.renderChart();
+          })
+          .catch((error) => {
+            console.error('请求失败示：', error);
+            // 在这里处理错误，比如提用户
+          });
+      } else {
+        axios
+          .get('http://127.0.0.1:3001/student2')
+          .then((response) => {
+            this.axesData = response.data;
+            console.log(this.mainMapData);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      // 发送 POST 请求到后端，携带按钮名称作为参数
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.common {
+  border: 3px solid #cad9f4;
+  border-radius: 10px;
+}
+
+.button-group {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  width: 130px;
+  height: 30px;
+  border: none;
+  background-color: #7099e5;
+  color: white;
+  border-radius: 10px;
+  font-size: 16px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  cursor: pointer;
+  margin: 5px;
+}
+button:hover {
+  background-color: #316fe2;
+}
+.layout-container {
+  display: flex;
+  padding: 5px;
+  margin-top: 65px;
+}
+
+.layout-column-left {
+  margin: 5px;
+  height: 1190px;
+}
+
+.layout-column-middle {
+  display: flex;
+  flex-direction: column;
+  margin: 0px 5px;
+  /* width: 800px; */
+}
+
+.layout-column-right {
+  display: flex;
+  flex-direction: column;
+}
+
+.layout-middle-top {
+  display: flex;
+  margin: 5px;
+  /* height: 6%; */
+}
+
+.layout-middle-middle {
+  /* flex: 1; */
+  margin: 5px;
+  /* height: 1000px; */
+}
+
+.layout-middle-bottom {
+  margin: 5px;
+  /* height: 10%; */
+}
+
+.layout-right-top,
+.layout-right-bottom {
+  display: flex;
+  /* flex: 1; */
+  /* margin: 5px; */
+}
+
+.layout-right-middle {
+  /* flex: 1; */
+  border: 3px solid #cad9f4;
+  margin: 5px;
+  border-radius: 10px;
+}
+
+.layout-right-top-left,
+.layout-right-top-right,
+.layout-right-bottom-left,
+.layout-right-bottom-right {
+  /* flex: 1; */
+  border: 3px solid #cad9f4;
+  margin: 5px;
+  border-radius: 10px;
+}
+
+.layout-right-top-left {
+  flex: 1;
+}
+
+.layout-right-top-right {
+  flex: 2;
 }
 </style>
